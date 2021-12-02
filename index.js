5ee657e52616ccedb9a5cb361dead54b11c1bc60
Kat <kyevsukov@gmail.com>
2021-12-02 15:09:25 -0500

    index finished

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express')
 
 const server = express()
 
+server.get('/api/users', (req, res) => {
+    res.json([
+        {id: 1, name: 'kat'},
+        {id: 2, name: 'clem'}
+    ])
+})
+
 const PORT = process.env.PORT || 9000
 
 server.listen(PORT, () => {
