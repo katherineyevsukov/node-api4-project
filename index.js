8df49282ad46fe010679c403682e28fb569d1246
Kat <kyevsukov@gmail.com>
2021-12-02 15:58:59 -0500

    test

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 const server = express()
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+const PORT = process.env.PORT || 9000
+
 
 server.get('/api/users', (req, res) => {
     res.json([
@@ -10,7 +16,10 @@ server.get('/api/users', (req, res) => {
     ])
 })
 
-const PORT = process.env.PORT || 9000
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
