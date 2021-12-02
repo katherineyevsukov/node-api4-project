a1d5e60ce67827a817aed0018bdefa27cf87cc1f
Kat <kyevsukov@gmail.com>
2021-12-02 18:07:25 -0500

    added basic endpoints

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
 
-const server = express()
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'client/build')))
+const server = express();
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9000;
 
+app.get("/", (req, res) => {
+  res.json({ message: "You have entered Kat's API" });
+});
 
-server.get('/api/users', (req, res) => {
-    res.json([
-        {id: 1, name: 'kat'},
-        {id: 2, name: 'clem'}
-    ])
-})
+app.get("/api/users", (req, res) => {
+  res.json({
+    users: [{ username: "Sam" }, { username: "Kat" }, { username: "Clem" }],
+  });
+});
 
-server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-})
+app.post("/api/register", (req, res) => {
+  const newUser = req.body;
+  res.status(201).json(newUser);
+});
 
+app.post("/api/login", (req, res) => {
+  if (req.body.username && req.body.password) {
+    res.status(200).json({ message: `Welcome ${req.body.username}` });
+  } else {
+    res.status(400).json({ message: "you are missing username or password" });
+  }
+});
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
